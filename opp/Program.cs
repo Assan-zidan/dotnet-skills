26efa3791091e09ac05c3e01b008d60652069383
assan zidan <nsangouassanzidan@gmail.com>
2025-03-05 17:59:47 +0100

    oriented object programming

---
+++ opp/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Animal 
+{
+    public virtual void MakeSound() 
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+public class Dog : Animal 
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
